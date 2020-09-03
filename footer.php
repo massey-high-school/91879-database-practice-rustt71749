eba7ad515c3ec2e2fdba555d9ecca24f91241902
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-09-03 12:20:18 +1200

    as

--- footer.php
+++ footer.php
@@ -1,27 +1,28 @@
  <div class="box side">
             
-            <h2>Search | <a class="side" href="show_all.php">Show All</a></h2>
+    <h2>Search | <a class="side" href="show_all.php">Show All</a></h2>
           
-            <i>Type Part of the Title / Author Name and then Search</i>
+    <i>Type Part of the Title / Author Name and then Search</i>
             
-            <hr />
+    <hr />
             
-            <!-- Start of Title Search -->
+    <!-- Start of Title Search -->
             
-            <form method="post" action="title_search.php" enctype="multipart/form-data">
+    <form method="post" action="title_search.php" enctype="multipart/form-data">
      
-                <input class="search" type="text" size="40" value="" required placeholder="Title ..."   />
-                
-                <input class="submit" type="submit" name="find_title" 
+        <input class="search" type="text" name="title" size="40" value="" required placeholder="Title ..."   />
+            
+        <input class="submit" type="submit" name="find_title" 
                        value="Search" />
-            </form>
+        
+    </form>
      
-            <!-- End of Title Search -->
+    <!-- End of Title Search -->
      
-            <hr />
-            Author Search<br />
-            Genre Search<br />
-            Rating Search
+    <hr />
+    Author Search<br />
+    Genre Search<br />
+    Rating Search
         
 </div>  <!-- / side bar -->
 
--- topbit.php
+++ topbit.php
@@ -6,6 +6,7 @@
     
     session_start();
     include("config.php");
+    include("functions.php"); // include data sanatising
     
     // Connect to database
     
