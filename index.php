12e09e897255a0d90755b6c4820cdd241ce88ac3
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-09-07 09:11:28 +1200

    changed title size

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         
         <div class="box main">
             
-            <h2>Welcome</h2>
+            <h1>Welcome</h1>
             
             <p>
                 Sugar plum powder dessert bonbon powder lollipop lollipop muffin I love. Tiramisu dessert I love sugar plum apple pie marzipan chocolate bar brownie. Pudding tiramisu caramels biscuit tiramisu. I love gummies gummies pie sweet pudding I love I love. Donut halvah candy oat cake donut. Caramels wafer danish jelly-o.
